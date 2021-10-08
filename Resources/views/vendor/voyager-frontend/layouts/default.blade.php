a3e987f169aa5ca4b5e3b73cda0892ca7ed63d60
Ahmad Syamim <ahmad.syamim@gmail.com>
2021-10-08 16:37:35 +0900

    features : check if exist theme class

--- Resources/views/vendor/voyager-frontend/layouts/default.blade.php
+++ Resources/views/vendor/voyager-frontend/layouts/default.blade.php
@@ -1,8 +1,12 @@
-@include('voyager-frontend::partials.meta')
-@include('voyager-frontend::partials.header')
+@if (class_exists('Theme') && Theme::exists(Theme::get()))
+    @include(Theme::get().".layouts.default")
+@else
+    @include('voyager-frontend::partials.meta')
+    @include('voyager-frontend::partials.header')
 
-<main class="main-content">
-    @yield('content')
-</main>
+    <main class="main-content">
+        @yield('content')
+    </main>
 
-@include('voyager-frontend::partials.footer')
+    @include('voyager-frontend::partials.footer')
+@endif
